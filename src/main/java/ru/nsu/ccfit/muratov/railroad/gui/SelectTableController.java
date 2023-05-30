aa50f8b00080f278b7288fc0dee3bc2255389f47
mu2so4 <b4.orangutang@gmail.com>
2023-05-30 14:24:56 +0700

    feat: Add layout to table selection

--- src/main/java/ru/nsu/ccfit/muratov/railroad/gui/SelectTableController.java
+++ src/main/java/ru/nsu/ccfit/muratov/railroad/gui/SelectTableController.java
@@ -1,4 +1,26 @@
 package ru.nsu.ccfit.muratov.railroad.gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class SelectTableController {
+    @FXML
+    private ListView listOfTables;
+
+    public void onBackButtonClick(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("main-menu.fxml"));
+        Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
