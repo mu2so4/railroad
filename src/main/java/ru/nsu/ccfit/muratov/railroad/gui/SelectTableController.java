b68a9667e1c4fc6d5a7e6222a117758801392815
mu2so4 <b4.orangutang@gmail.com>
2023-05-30 17:18:36 +0700

    feat: Add fetching of table list

--- src/main/java/ru/nsu/ccfit/muratov/railroad/gui/SelectTableController.java
+++ src/main/java/ru/nsu/ccfit/muratov/railroad/gui/SelectTableController.java
@@ -8,12 +8,28 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+import ru.nsu.ccfit.muratov.railroad.database.DatabaseException;
+import ru.nsu.ccfit.muratov.railroad.database.table.Table;
+import ru.nsu.ccfit.muratov.railroad.database.table.TableListReader;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.List;
 
 public class SelectTableController {
     @FXML
     private ListView listOfTables;
+
+    public SelectTableController() throws IOException, SQLException, DatabaseException {
+        TableListReader reader = new TableListReader();
+        List<Table> list = reader.loadTableList();
+        var items = listOfTables.getItems();
+        for(Table table: list) {
+            items.add(table.getName());
+        }
+    }
 
     public void onBackButtonClick(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("main-menu.fxml"));
