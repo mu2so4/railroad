fd51084aeb9a42261c68bd714dfc2d76c18d7524
mu2so4 <b4.orangutang@gmail.com>
2023-05-30 18:15:28 +0700

    fix: Move initialization into MainController

--- src/main/java/ru/nsu/ccfit/muratov/railroad/gui/SelectTableController.java
+++ src/main/java/ru/nsu/ccfit/muratov/railroad/gui/SelectTableController.java
@@ -8,28 +8,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
-import ru.nsu.ccfit.muratov.railroad.database.DatabaseException;
-import ru.nsu.ccfit.muratov.railroad.database.table.Table;
-import ru.nsu.ccfit.muratov.railroad.database.table.TableListReader;
 
 import java.io.IOException;
-import java.net.URL;
-import java.sql.SQLException;
-import java.util.Iterator;
-import java.util.List;
 
 public class SelectTableController {
     @FXML
     private ListView listOfTables;
-
-    public SelectTableController() throws IOException, SQLException, DatabaseException {
-        TableListReader reader = new TableListReader();
-        List<Table> list = reader.loadTableList();
-        var items = listOfTables.getItems();
-        for(Table table: list) {
-            items.add(table.getName());
-        }
-    }
 
     public void onBackButtonClick(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("main-menu.fxml"));
@@ -38,5 +22,9 @@ public class SelectTableController {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public ListView getListOfTables() {
+        return listOfTables;
     }
 }
